e221d5d7b8f7b7fe358dde4aba43b465a552379f
Michele Gargiulo <michele@grg.io>
2023-07-24 14:30:11 +0200

    Adds a shortcut for finding the string under the cursor in cwd.

--- nvim/.config/nvim/lua/plugins/search.lua
+++ nvim/.config/nvim/lua/plugins/search.lua
@@ -11,6 +11,7 @@ return {
         },
         keys = {
             {"<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files"},
+            {"<leader>*", "<Cmd>Telescope grep_string<CR>", desc = "Find the string under cursor"},
             {"<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep"},
             {"<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find buffers"},
             {"<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Find help tags"},
