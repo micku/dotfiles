edb40cd3c7fdc3069fd76a151b7cecbceaaf3349
Michele Gargiulo <michele@grg.io>
2023-07-24 14:29:19 +0200

    Creates an autocmd to attach JDTLS on FileType event. Solves #6.

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -92,58 +92,63 @@ return {
         },
         ft = "java",
         config = function()
-            local home = os.getenv('HOME')
-            local jdtls = require('jdtls')
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "java",
+                callback = function()
+                    local home = os.getenv('HOME')
+                    local jdtls = require('jdtls')
 
-            -- TODO: This path is useful in multiple context(ie. format.lua) and may be extracted
-            local mason_packages_path = vim.fn.stdpath "data" .. "/mason/packages/"
-            local jdtls_path = mason_packages_path .. "jdtls/"
-            local java_debug_path = mason_packages_path .. "java-debug-adapter/"
-            local java_test_path = mason_packages_path .. "java-test/"
+                    -- TODO: This path is useful in multiple context(ie. format.lua) and may be extracted
+                    local mason_packages_path = vim.fn.stdpath "data" .. "/mason/packages/"
+                    local jdtls_path = mason_packages_path .. "jdtls/"
+                    local java_debug_path = mason_packages_path .. "java-debug-adapter/"
+                    local java_test_path = mason_packages_path .. "java-test/"
 
-            local root_dir = vim.fn.getcwd()
+                    local root_dir = vim.fn.getcwd()
 
-            local workspace_folder = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+                    local workspace_folder = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
 
-            local config = {
-                flags = {
-                    debounce_text_changes = 80,
-                },
-                -- cmd = {jdtls_path .. "bin/jdtls"},
-                cmd = {
-                    "java",
-                    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-                    '-Dosgi.bundles.defaultStartLevel=4',
-                    '-Declipse.product=org.eclipse.jdt.ls.core.product',
-                    '-Dlog.protocol=true',
-                    '-Dlog.level=ALL',
-                    '-Xmx4g',
-                    '--add-modules=ALL-SYSTEM',
-                    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-                    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-                    '-javaagent:' .. jdtls_path .. 'lombok.jar',
-                    '-jar', vim.fn.glob(jdtls_path .. 'plugins/org.eclipse.equinox.launcher_*.jar'),
-                    '-configuration', jdtls_path .. 'config_linux',
-                    '-data', workspace_folder,
-                },
-                root_dir = root_dir,
+                    local config = {
+                        flags = {
+                            debounce_text_changes = 80,
+                        },
+                        -- cmd = {jdtls_path .. "bin/jdtls"},
+                        cmd = {
+                            "java",
+                            '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+                            '-Dosgi.bundles.defaultStartLevel=4',
+                            '-Declipse.product=org.eclipse.jdt.ls.core.product',
+                            '-Dlog.protocol=true',
+                            '-Dlog.level=ALL',
+                            '-Xmx4g',
+                            '--add-modules=ALL-SYSTEM',
+                            '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+                            '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+                            '-javaagent:' .. jdtls_path .. 'lombok.jar',
+                            '-jar', vim.fn.glob(jdtls_path .. 'plugins/org.eclipse.equinox.launcher_*.jar'),
+                            '-configuration', jdtls_path .. 'config_linux',
+                            '-data', workspace_folder,
+                        },
+                        root_dir = root_dir,
 
-                settings = {
-                    java = {
-                        signatureHelp = { enabled = true },
-                        contentProvider = { preferred = 'fernflower' },
-                    },
-                },
+                        settings = {
+                            java = {
+                                signatureHelp = { enabled = true },
+                                contentProvider = { preferred = 'fernflower' },
+                            },
+                        },
 
-                -- TODO: These are debugging configurations, those should stay in debugger.lua
-                init_options = {
-                    bundles = {
-                        vim.fn.glob(java_debug_path .. "extension/server/com.microsoft.java.debug.plugin-*.jar", true),
-                        vim.fn.glob(java_test_path .. "extension/server/*.jar", true),
+                        -- TODO: These are debugging configurations, those should stay in debugger.lua
+                        init_options = {
+                            bundles = {
+                                vim.fn.glob(java_debug_path .. "extension/server/com.microsoft.java.debug.plugin-*.jar", true),
+                                vim.fn.glob(java_test_path .. "extension/server/*.jar", true),
+                            }
+                        },
                     }
-                },
-            }
-            jdtls.start_or_attach(config)
+                    jdtls.start_or_attach(config)
+                end
+            })
         end,
         keys = {
             {"<leader>df", "<cmd>lua require'jdtls'.test_class()<cr>", desc = "Debug JUnit test class"},
