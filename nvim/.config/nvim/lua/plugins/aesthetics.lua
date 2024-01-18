a3e691b4d3eb88cf1d38d6451acc7a487bde61fe
Michele Gargiulo <michele@grg.io>
2024-01-18 08:35:53 +0100

    Switch to Rosé Pine theme.

--- nvim/.config/nvim/lua/plugins/aesthetics.lua
+++ nvim/.config/nvim/lua/plugins/aesthetics.lua
@@ -1,55 +1,42 @@
 return {
     -- the colorscheme should be available when starting Neovim
     {
-        "folke/tokyonight.nvim",
+        "rose-pine/neovim",
+        name = "rose-pine",
         lazy = false, -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
-            vim.opt.termguicolors = true
+            require("rose-pine").setup({
+                highlight_groups = {
+                    -- Remove virtual text background
+                    DiagnosticVirtualTextError = { bg = "none" },
+                    DiagnosticVirtualTextHint = { bg = "none" },
+                    DiagnosticVirtualTextInfo = { bg = "none" },
+                    DiagnosticVirtualTextWarn = { bg = "none" },
 
-            require("tokyonight").setup({
-                on_highlights = function(hl, c)
-                    local prompt = "#2d3149"
-                    hl.TelescopeNormal = {
-                        bg = c.bg_dark,
-                        fg = c.fg_dark,
-                    }
-                    hl.TelescopeBorder = {
-                        bg = c.bg_dark,
-                        fg = c.bg_dark,
-                    }
-                    hl.TelescopePromptNormal = {
-                        bg = prompt,
-                    }
-                    hl.TelescopePromptBorder = {
-                        bg = prompt,
-                        fg = prompt,
-                    }
-                    hl.TelescopePromptTitle = {
-                        bg = prompt,
-                        fg = prompt,
-                    }
-                    hl.TelescopePreviewTitle = {
-                        bg = c.bg_dark,
-                        fg = c.bg_dark,
-                    }
-                    hl.TelescopeResultsTitle = {
-                        bg = c.bg_dark,
-                        fg = c.bg_dark,
-                    }
-                end,
+                    -- Borderless Telescope
+                    TelescopeBorder = { fg = "surface", bg = "surface" },
+                    TelescopeNormal = { fg = "subtle", bg = "overlay" },
+                    TelescopeTitle = { fg = "base", bg = "love" },
+
+                    TelescopeResultsBorder = { fg = "overlay", bg = "overlay" },
+                    TelescopeResultsNormal = { fg = "text", bg = "overlay" },
+
+                    TelescopeSelection = { fg = "text", bg = "highlight_med" },
+                    TelescopeSelectionCaret = { fg = "love", bg = "highlight_med" },
+                    TelescopeMultiSelection = { fg = "text", bg = "highlight_high" },
+
+                    TelescopePreviewTitle = { fg = "base", bg = "iris" },
+                    TelescopePreviewBorder = { fg = "overlay", bg = "overlay" },
+                    TelescopePreviewNormal = { bg = "overlay" },
+
+                    TelescopePromptTitle = { fg = "base", bg = "pine" },
+                    TelescopePromptNormal = { fg = "text", bg = "surface" },
+                    TelescopePromptBorder = { fg = "surface", bg = "surface" },
+                },
             })
 
-            -- load the colorscheme here
-            vim.cmd([[colorscheme tokyonight-moon]])
-
-            -- Set some colors
-            vim.cmd "highlight Search ctermbg=4"
-            vim.cmd "highlight LineNr ctermfg=8"
-            vim.cmd "highlight CursorLineNr ctermfg=5"
-            vim.cmd "highlight Comment cterm=italic ctermfg=8"
-            vim.cmd "highlight Todo ctermbg=8 ctermfg=6"
-            vim.cmd "highlight Statement cterm=bold ctermfg=15"
-        end,
+            vim.cmd("colorscheme rose-pine")
+        end
     },
 }
--- nvim/.config/nvim/lua/plugins/statusline.lua
+++ nvim/.config/nvim/lua/plugins/statusline.lua
@@ -7,7 +7,6 @@ return {
         require("lualine").setup({
             options = {
                 icons_enabled = true,
-                theme = 'tokyonight',
                 component_separators = { left = '', right = '|'},
                 section_separators = { left = '', right = ''},
                 disabled_filetypes = {
