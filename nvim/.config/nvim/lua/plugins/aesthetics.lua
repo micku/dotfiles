d1b09f718edb9f7ac3f900dc59ca5446cef08f0a
Michele Gargiulo <michele.gargiulo@klarna.com>
2024-09-30 15:39:18 +0200

    Change theme to Catppuccin

--- nvim/.config/nvim/lua/plugins/aesthetics.lua
+++ nvim/.config/nvim/lua/plugins/aesthetics.lua
@@ -1,49 +1,28 @@
 return {
     -- the colorscheme should be available when starting Neovim
     {
-        "rose-pine/neovim",
-        name = "rose-pine",
+        "catppuccin/nvim",
+        name = "catppuccin",
         lazy = false, -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
-            require("rose-pine").setup({
-                dim_inactive_windows = true,
-
-                styles = {
-                    italic = false,
-                    bold = false,
-                },
-
-                highlight_groups = {
-                    -- Remove virtual text background
-                    DiagnosticVirtualTextError = { bg = "none" },
-                    DiagnosticVirtualTextHint = { bg = "none" },
-                    DiagnosticVirtualTextInfo = { bg = "none" },
-                    DiagnosticVirtualTextWarn = { bg = "none" },
-
-                    -- Borderless Telescope
-                    TelescopeBorder = { fg = "surface", bg = "surface" },
-                    TelescopeNormal = { fg = "subtle", bg = "overlay" },
-                    TelescopeTitle = { fg = "base", bg = "love" },
-
-                    TelescopeResultsBorder = { fg = "overlay", bg = "overlay" },
-                    TelescopeResultsNormal = { fg = "text", bg = "overlay" },
-
-                    TelescopeSelection = { fg = "text", bg = "highlight_med" },
-                    TelescopeSelectionCaret = { fg = "love", bg = "highlight_med" },
-                    TelescopeMultiSelection = { fg = "text", bg = "highlight_high" },
-
-                    TelescopePreviewTitle = { fg = "base", bg = "iris" },
-                    TelescopePreviewBorder = { fg = "overlay", bg = "overlay" },
-                    TelescopePreviewNormal = { bg = "overlay" },
-
-                    TelescopePromptTitle = { fg = "base", bg = "pine" },
-                    TelescopePromptNormal = { fg = "text", bg = "surface" },
-                    TelescopePromptBorder = { fg = "surface", bg = "surface" },
+            require("catppuccin").setup({
+                integrations = {
+                    lsp_trouble = true,
+                    telescope = {
+                        enabled = true,
+                        style = "nvchad",
+                    },
                 },
             })
 
-            vim.cmd("colorscheme rose-pine")
+            local sign = vim.fn.sign_define
+
+            sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = ""})
+            sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = ""})
+            sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = ""})
+
+            vim.cmd("colorscheme catppuccin-mocha")
         end
     },
 }
