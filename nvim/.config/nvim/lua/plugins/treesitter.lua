eb45761ecd8f2a02d3bf37aca26b22423e7feaa8
Michele Gargiulo <michele.gargiulo@klarna.com>
2024-11-12 18:34:25 +0100

    Fixes :h treesitter errors

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
     event = "BufRead",
     config = function(_, opts)
         opts.ensure_installed = opts.ensure_installed or {}
+        vim.list_extend(opts.ensure_installed, {"vimdoc", "luadoc", "vim"})
 
         require("nvim-treesitter.configs").setup {
             ensure_installed = opts.ensure_installed,
