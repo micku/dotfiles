965244a09e0368453f4bdf232ec75f0f5792ff42
Michele Gargiulo <michele.gargiulo@klarna.com>
2025-02-06 15:41:18 +0100

    Some changes in Copilot configuration

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -74,27 +74,32 @@ return {
     },
     {
         "CopilotC-Nvim/CopilotChat.nvim",
-        branch = "canary",
+        dependencies = {
+            "zbirenbaum/copilot.lua",
+            { "nvim-lua/plenary.nvim", branch = "master" },
+        },
+        branch = "main",
         opts = {
             show_help = "yes", -- Show help text for CopilotChatInPlace, default: yes
             debug = false, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
-            disable_extra_info = 'no', -- Disable extra information (e.g: system prompt) in the response.
+            disable_extra_info = "no", -- Disable extra information (e.g: system prompt) in the response.
+            model = "claude-3.5-sonnet",
         },
         build = function()
             vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
         end,
         event = "VeryLazy",
         keys = {
-            { "<leader>cce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
-            { "<leader>cct", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
+            { "<leader>ce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
+            { "<leader>ct", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests" },
             {
-                "<leader>ccv",
+                "<leader>cv",
                 ":CopilotChatVisual",
                 mode = "x",
                 desc = "CopilotChat - Open in vertical split",
             },
             {
-                "<leader>ccx",
+                "<leader>cx",
                 ":CopilotChatInPlace<cr>",
                 mode = "x",
                 desc = "CopilotChat - Run in-place code",
