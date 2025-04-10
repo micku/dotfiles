36712bc3ddf77dd99d687cc0734889bfbfde76f7
Michele Gargiulo <michele.gargiulo@klarna.com>
2025-04-11 00:09:48 +0200

    Fixes scrolloff and adds sidescrolloff

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -32,8 +32,9 @@ vim.o.viminfo = "'100,f1"
 -- Screen will not be redrawn while running macros, registers or other non-typed comments
 vim.o.lazyredraw = true
 
--- Keep the cursor visible within 3 lines when scrolling
-vim.wo.scrolloff = 3
+-- Keep the cursor visible within 3 lines/columns when scrolling
+vim.o.scrolloff = 3
+vim.o.sidescrolloff = 3
 
 -- Indentation
 vim.bo.expandtab = true       -- use spaces instead of tabs
