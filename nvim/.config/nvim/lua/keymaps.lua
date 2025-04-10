441de23985b4afa6d7bf9691249bcfe2b1656c3b
Michele Gargiulo <michele.gargiulo@klarna.com>
2025-04-10 23:46:17 +0200

    Removes bindings replaced by 0.11 defaults Uses virtual lines for diagnostics

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -21,11 +21,3 @@ map("n", "k", "gk", {silent = true, desc = "Move up by displayed lines"})
 map("n", "j", "gj", {silent = true, desc = "Move down by displayed lines"})
 map("n", "0", "g0", {silent = true, desc = "Move to the beginning of the displayed line"})
 map("n", "$", "g$", {silent = true, desc = "Move to the end of the displayed line"})
-
--- Diagnostics
-map("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", { desc = "Go to prev diagnostic", noremap=true, silent=true })
-map("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>", { desc = "Go to next diagnostic", noremap=true, silent=true })
-
--- LSP
-map("n", "gr", "<cmd>lua vim.lsp.buf.rename()<cr>", { desc = "Rename", noremap=true, silent=true })
-map("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", { desc = "Code actions", noremap=true, silent=true })
--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -1,3 +1,14 @@
+vim.diagnostic.config({
+  -- Use the default configuration
+  -- virtual_lines = true
+
+  -- Alternatively, customize specific options
+  virtual_lines = {
+   -- Only show virtual line diagnostics for the current cursor line
+   current_line = true,
+  },
+})
+
 return {
     "tpope/vim-sleuth",
     {
